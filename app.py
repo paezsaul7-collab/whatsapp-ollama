439adbc91a2271d461a8f90d6fe2919271ae1601
paezsaul7-collab <paezsaul7@gmail.com>
2026-06-07 22:36:22 -0600

    fix port railway

--- app.py
+++ app.py
@@ -78,4 +78,6 @@ def RecibirMensaje():
         return "EVENT_RECEIVED"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8502, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8502))
+    app.run(host="0.0.0.0", port=port, debug=True)
